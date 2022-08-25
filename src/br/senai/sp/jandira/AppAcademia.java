260c6e6e80eb73804ce12779924c0dae5413f794
ÍTALO REIS ROSA DA SILVA <22282094@CFP127ALUNO.LOCAL>
2022-08-25 11:13:50 -0300

    versao inicial + uso do Click listener

--- src/br/senai/sp/jandira/AppAcademia.java
+++ src/br/senai/sp/jandira/AppAcademia.java
@@ -1,6 +1,5 @@
 package br.senai.sp.jandira;
 
-
 import java.awt.Color;
 import java.time.LocalDate;
 
@@ -14,7 +13,7 @@ import br.senai.sp.jandira.model.Cliente;
 public class AppAcademia {
 
 	public static void main(String[] args) {
-		
+
 		FrameCalculoImc tela = new FrameCalculoImc();
 		tela.titulo = "Calculadora Imc";
 		tela.altura = 600;
@@ -23,35 +22,29 @@ public class AppAcademia {
 		tela.corDobotao = new Color(79, 79, 255);
 		tela.corDoTexto = new Color(248, 224, 58);
 		tela.criarTela();
-		
-		Cliente ana = new Cliente(); //"new client" reserve a space in the ram memory,so we have to attribute it something.
-		Cliente pedro = new Cliente();//example in the line 17,18,20,21
+
+		Cliente ana = new Cliente(); // "new client" reserve a space in the ram memory,so we have to attribute it
+										// something.
+		Cliente pedro = new Cliente();// example in the line 17,18,20,21
 		System.out.println(ana);
 		System.out.println(pedro);
-	
-		
+
 		ana.nome = "Ana Maria Brega";
 		ana.dataDeNascimento = LocalDate.of(2005, 9, 16);
 		ana.altura = 1.69;
 		ana.peso = 55;
-	
-		
-	
-		
-		
-		
-		
-	    ana.getimc();
-	    System.out.println(ana.getimc());
-	    ana.getStatus();
-	    String teste = ana.getStatus();
-	  //  JOptionPane.showMessageDialog(null, teste);
-	    //we call "windows" as frame, CAUSE WE ARE NOT DUMB USERSSSS
-	    //we call the "main part/intern" of a frame as container
-	    //Library used to create frames: JFrame //Jlabel = exactly a label //JtextField = used to create text field
-	    // (30,50,40,30) eixo x, eixo y, width, heigth
-	    
-	    
+
+		ana.getimc();
+		System.out.println(ana.getimc());
+		ana.getStatus();
+		String teste = ana.getStatus();
+		// JOptionPane.showMessageDialog(null, teste);
+		// we call "windows" as frame, CAUSE WE ARE NOT DUMB USERSSSS
+		// we call the "main part/intern" of a frame as container
+		// Library used to create frames: JFrame //Jlabel = exactly a label //JtextField
+		// = used to create text field
+		// (30,50,40,30) eixo x, eixo y, width, heigth
+
 	}
 
 }
--- src/br/senai/sp/jandira/model/Cliente.java
+++ src/br/senai/sp/jandira/model/Cliente.java
@@ -4,52 +4,41 @@ import java.time.LocalDate;
 
 //class are like "recipe" to make objects
 
-
 public class Cliente {
 
-	//here we have "everything" that is important to our project, based on each necessities/characteristics
-public String nome;
-public LocalDate dataDeNascimento;
-public String genero[] = {"Feminino", "Masculino", "Outros" } ;
-public double peso;
-public double altura;
-
-
+	// here we have "everything" that is important to our project, based on each
+	// necessities/characteristics
+	public String nome;
+	public LocalDate dataDeNascimento;
+	public String genero[] = { "Feminino", "Masculino", "Outros" };
+	public double peso;
+	public double altura;
 
 //methods/function = behavior
 //tip: use library called Math
-public double getimc() {
-	return  peso/ Math.pow(altura,2);
-	
-	
-}
+	public double getimc() {
+		return peso / Math.pow(altura, 2);
 
-public String getStatus() {
-	
-	if (getimc() <= 18.5) {
-		return"Abaixo Do Peso";
+	}
+
+	public String getStatus() {
+
+		if (getimc() <= 18.5) {
+			return "Abaixo Do Peso";
 		} else if (getimc() >= 18.50 && getimc() < 25.00) {
-			return "Peso Ideal";
-			
-		}
-		else if (getimc() > 25.00 && getimc() < 30.00) {
+			return "Peso Ideal !";
+
+		} else if (getimc() > 25.00 && getimc() < 30.00) {
 			return "Levemente acima do peso";
-			
-		}
-		else if (getimc() > 30.00 && getimc()< 35.00) {
+
+		} else if (getimc() > 30.00 && getimc() < 35.00) {
 			return "Obesiade grau 1 ";
-		}
-		else if (getimc() > 35 && getimc() < 40.00) {
+		} else if (getimc() > 35 && getimc() < 40.00) {
 			return "Obesidade grau 2";
+		} else {
+			return "Obesidade Mórbida";
 		}
-		else {
-			return"Obesidade Mórbida";
-		}
-		
-	
-	
-	
-	
+
 //	if (getimc() < 18.50) { System.out.print(" Você está abaixo do peso");	
 //	}
 //	if (getimc() >= 18.50 && getimc() < 25.00 ) { System.out.print("Você está no peso ideal");
@@ -62,8 +51,7 @@ public String getStatus() {
 //	}
 //	if (getimc() >= 40.00) { System.out.print("Você está com Obesidade 3 (mórbida)");
 //	}
-	
-}
 
-	
+	}
+
 }
